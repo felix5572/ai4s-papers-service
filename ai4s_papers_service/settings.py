c776108b6801513839b3c10d18ab1843d6464496
felix5572 <yfb222333@gmail.com>
2025-06-07 18:31:07 -0500

    update database use zeabur

--- ai4s_papers_service/settings.py
+++ ai4s_papers_service/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'ai4s_papers_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
