5352c1722b196f09fe8656941fc2a7f424e4b8cf
felix5572 <yfb222333@gmail.com>
2025-06-08 22:40:59 -0500

    prefect serve multi flow

---
+++ prefect_workflow/hello_world.py
@@ -0,0 +1,9 @@
+from prefect import flow
+
+
+@flow(log_prints=True)
+def hello_world(name: str = "world", goodbye: bool = False):
+    print(f"Hello {name} from Prefect!")
+
+    if goodbye:
+        print(f"Goodbye {name}!")
