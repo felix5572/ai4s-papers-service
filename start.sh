e7e149c9b8c487f3cd30ab13acceef2f5406adef
felix5572 <yfb222333@gmail.com>
2025-07-13 15:01:56 -0500

    fix start command

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --noinput
 
+
 # Start server
 gunicorn ai4s_papers_service.wsgi:application --bind 0.0.0.0:${PORT:-8080} 
